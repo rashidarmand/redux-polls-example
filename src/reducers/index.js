2f09a564d0d27f30b793bc333670f05db57bb191
Rashid H Armand <rashidharmand@gmail.com>
2019-01-13 19:26:16 -0500

    reducers and store created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,10 @@
 import users from './users'
 import authedUser from './authedUser'
 import polls from './polls'
+import { combineReducers } from 'redux';
 
+export default combineReducers({
+  authedUser,
+  users,
+  polls
+})
