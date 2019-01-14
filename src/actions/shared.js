7068bac1e904b61624784efc4f728becd40fe30c
Rashid H Armand <rashidharmand@gmail.com>
2019-01-13 19:14:41 -0500

    actions + action creators done

---
+++ src/actions/shared.js
@@ -0,0 +1,15 @@
+import { getInitialData } from '../utils/api'
+import { receiveUsers } from './users'
+import { receivePolls } from './polls'
+import { setAuthedUser } from './authedUser'
+
+const AUTHED_ID = 'dan_abramov'
+
+export function handleInitialData() {
+  return (dispatch) => getInitialData()
+    .then(({ users, polls }) => {
+      dispatch(receiveUsers(users))
+      dispatch(receivePolls(polls))
+      dispatch(setAuthedUser(AUTHED_ID))
+    })
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+import users from './users'
+import authedUser from './authedUser'
+import polls from './polls'
+
